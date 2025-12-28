c18ca5704275a9053985de190f4bb2952ebf3405
Mick-Mick01 <devkha8721@gmail.com>
2025-12-28 16:19:43 +0530

    Made the projectview optimised for mobiles

--- app/dashboard/myMongo.py
+++ app/dashboard/myMongo.py
@@ -4,7 +4,8 @@ from bson import Binary
 Atlas_string1 = "mongodb+srv://dev3kha7_8721:YWzwlBcc4swtZEqN@1stcluster.ldsbsgi.mongodb.net/"
 Atlas_string2 = "mongodb://dev3kha7_8721:YWzwlBcc4swtZEqN@ac-0x74grp-shard-00-00.ldsbsgi.mongodb.net:27017,ac-0x74grp-shard-00-01.ldsbsgi.mongodb.net:27017,ac-0x74grp-shard-00-02.ldsbsgi.mongodb.net:27017/?ssl=true&replicaSet=atlas-74grp-shard-0&authSource=admin&retryWrites=true&w=majority&appName=1stCluster"
 
-local_client = pymongo.MongoClient(Atlas_string1)
+# local_client = pymongo.MongoClient(Atlas_string1)
+local_client = pymongo.MongoClient("mongodb://localhost:27017")
 local_db = local_client['PortFolio']
 
 
--- app/dashboard/routes.py
+++ app/dashboard/routes.py
@@ -6,7 +6,8 @@ from bson import Binary
 import gridfs #To store DOCs and give users for download through Return send_files()
 
 Atlas_string1 = "mongodb+srv://dev3kha7_8721:YWzwlBcc4swtZEqN@1stcluster.ldsbsgi.mongodb.net/"
-local_client = pymongo.MongoClient(Atlas_string1)
+# local_client = pymongo.MongoClient(Atlas_string1)
+local_client = pymongo.MongoClient("mongodb://localhost:27017")
 local_db = local_client['PortFolio']
 fsDB = local_client["PortFolio-Confidential"]
 fs = gridfs.GridFS(fsDB)
@@ -94,8 +95,7 @@ def addProject(memberCode):
             for cat in categories:
                 colection = local_db[cat]
                 project1 = colection.find_one({"projectCode":projectCode})
-                project2 = colection.find_one({"projectName":projectName})
-                if project1 or project2:
+                if project1:
                     return "A project with this Project-Code already exists. Please change the name & Project-Code or delete the older project"
                 
 #           checking for XSS atack on a flask Jinja-template environment
