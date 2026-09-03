2c3c6008729a0c56d1a25e248b866e21c812611e
Mick-Mick01 <devkha8721@gmail.com>
2026-09-04 04:17:51 +0530

    Resolved the email sendind issue. Emails are now send in the background on another thread so the user does not see the reloading screen anymore.

--- app/dashboard/routes.py
+++ app/dashboard/routes.py
@@ -4,8 +4,12 @@ import pymongo
 from . import dashboard_bp
 from bson import Binary
 import gridfs #To store DOCs and give users for download through Return send_files()
+from dotenv import load_dotenv
+import os
 
-Atlas_string1 = "mongodb+srv://dev3kha7_8721:YWzwlBcc4swtZEqN@1stcluster.ldsbsgi.mongodb.net/"
+load_dotenv()
+
+Atlas_string1 = os.getenv("Atlas_string1")
 local_client = pymongo.MongoClient(Atlas_string1)
 # local_client = pymongo.MongoClient("mongodb://localhost:27017")
 local_db = local_client['PortFolio']
